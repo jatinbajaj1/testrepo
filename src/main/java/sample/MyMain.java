8d2f3fc1e1170e917bb70c58520991472205d810
jatin <jatinbajaj.mca@gmail.com>
2017-01-06 15:27:00 +0530

    adding new project

---
+++ src/main/java/sample/MyMain.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class MyMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world how r u");
+		
+		String abc = null;
+		abc.compareTo("jatin");
+	}
+
+}
