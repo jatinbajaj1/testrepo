937ccc74fb522a2b76e15f116760131dd82b9750
PDS <jatin@email.com>
2017-01-13 10:05:14 +0530

    try

--- ViewBooks.java
+++ ViewBooks.java
@@ -30,7 +30,7 @@ public class ViewBooks extends JFrame {
 	}
 
 	/*
-	 * Create the frame. ppppp
+	 * Create the frame. p
 	 */
 	public ViewBooks() {
 		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
